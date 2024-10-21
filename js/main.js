8f17330127b4a11cd87d1fef886bae7f7b0561e9
Victor Schweitz <victorschweitz@hotmail.com>
2024-10-21 14:11:48 +0200

    Nothing

--- js/main.js
+++ js/main.js
@@ -3,27 +3,26 @@ const todoAppContainer = document.querySelector('.todo-app-container')
 // EVENT LISTENERS //
 todoAppContainer.addEventListener('click', (e) =>
 {
-    const completeBtn = e.target.closest('.complete-todo-btn');
-    const deleteBtn = e.target.closest('.delete-todo-btn');
+    const completeBtn = e.target.closest('.complete-todo-btn')
+    const deleteBtn = e.target.closest('.delete-todo-btn')
 
     if (completeBtn)
     {
-        completeTodo(completeBtn);
+        completeTodo(completeBtn)
     }
     else if (deleteBtn)
     {
-        deleteTodo(deleteBtn);
+        deleteTodo(deleteBtn)
     }
-});
+})
 
 function completeTodo(btn)
 {
-    btn.closest('.todo-list-item').classList.add('completed');
-    btn.classList.add('completed');
+    btn.closest('.todo-list-item').classList.add('completed')
+    btn.classList.add('completed')
 }
 
 // Delete Todo
-function deleteTodo(e)
+function deleteTodo(btn)
 {
-
 }
